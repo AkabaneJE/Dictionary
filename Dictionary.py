a711c194f6360aa1a9d5554ed3346f1d82d31eb6
Seng How Lee <enjiasasuke@gmail.com>
2023-08-06 10:32:06 +0800

    Uploading Dictionary

---
+++ Dictionary.py
@@ -0,0 +1,6 @@
+Dictionary ={
+"Conditionals":"Allow your code to execute different actions depending on whether condition is true or false",
+"Functions":"Perfrom a specific task,allowing breaking down a code into smaller, reusable pieces",
+"Loops":"Repeats a block of code a certain number of times or until specific condition is met"}
+
+print(Dictionary["Conditionals"])
